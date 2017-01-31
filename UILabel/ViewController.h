063dcad270fb3b0e4284d0a8e2748b0d6e2fec15
ferrakkem <ferrakkem@gmail.com>
2017-01-31 14:46:14 +0600

    Initial Commit

---
+++ UILabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILabel
+//
+//  Created by Ferrakkem Bhuiyan on 31/1/17.
+//  Copyright © 2017 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UILabel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UILabel
+//
+//  Created by Ferrakkem Bhuiyan on 31/1/17.
+//  Copyright © 2017 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
